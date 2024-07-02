422185ea762dc9baa110d2ffd47e1651eb9cea77
jhonpaulcorullo <jhonpaulcorullo@gmail.com>
2024-07-02 11:33:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,18 +4,6 @@ var price1 = document.getElementById("price1");
 var product2 = document.getElementById("product2");
 var qty2 = document.getElementById("qty2");
 var price2 = document.getElementById("price2");
-var product3 = document.getElementById("product3");
-var qty3 = document.getElementById("qty3");
-var price3 = document.getElementById("price3");
-var product4 = document.getElementById("product4");
-var qty4 = document.getElementById("qty4");
-var price4 = document.getElementById("price4");
-var product5 = document.getElementById("product5");
-var qty5 = document.getElementById("qty5");
-var price5 = document.getElementById("price5");
-var product6 = document.getElementById("product6");
-var qty6 = document.getElementById("qty6");
-var price6 = document.getElementById("price6");
 var carts = document.getElementById("carts");
 var totalInput = document.getElementById("total");
 var cashInput = document.getElementById("cash");
@@ -62,8 +50,4 @@ function calculateChange() {
 
 qty1.addEventListener("keyup", addOrder);
 qty2.addEventListener("keyup", addOrder);
-qty3.addEventListener("keyup", addOrder);
-qty4.addEventListener("keyup", addOrder);
-qty5.addEventListener("keyup", addOrder);
-qty6.addEventListener("keyup", addOrder);
 cashInput.addEventListener("input", calculateChange);
